af8b86092d499d4900dc30b9e8cbb10909001b56
Jose Carlos Tejada <jctejadasaracho@gmail.com>
2015-06-13 21:11:28 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -73,3 +73,32 @@ exports.create = function(req, res) {
 			}
 		});
 };
+
+// GET /quizes/:quizId/edit
+exports.edit = function(req, res) {
+	var quiz = req.quiz; // autoload quiz instance
+
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+// PUT /quizes/:quizId
+exports.update = function(req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz
+	.validate()
+	.then(
+		function(err) {
+			if (err) {
+				res.render('quizes/edit', {quiz: quiz, errors: err.errors});
+			}
+			else {
+				req.quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then(function() {
+					res.redirect('/quizes'); // HTTP redirect to questions list
+				});
+			}
+		});	
+}
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,9 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 // Create
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
+// Edit
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 
 // GET author page.
 router.get('/author', function(req, res) {
